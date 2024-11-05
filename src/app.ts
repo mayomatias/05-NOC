82c3146efd9e6e1092259296a8b5c2f54a8bbff9
mayomatias <mayomatiass@gmail.com>
2024-11-05 22:13:46 +0100

    Fin seccion 9 NOC App

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
+import { log } from 'console';
 import { Server } from './presentation/server';
+import { envs } from './config/envs.plugin';
 
 (async() =>  {
 
@@ -7,5 +9,7 @@ import { Server } from './presentation/server';
 })();
 
 function main(){
-    Server.start();
+   // Server.start();
+   console.log(envs);
+   
 }
